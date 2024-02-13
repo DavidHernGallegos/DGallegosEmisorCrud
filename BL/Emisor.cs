977823db2f4dc5f27cf492e6b1870898f800d551
DavidHernGallegos <david.herngallegos@gmail.com>
2024-02-12 19:24:26 -0600

    Add project files.

---
+++ BL/Emisor.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Emisor
+    {
+       public static  Dictionary<string,object> GetAll()
+        {
+           ML.Emisor emisor = new ML.Emisor();
+           Dictionary<string,object> diccionario = new Dictionary<string, object> { {"Emisor ", emisor },{"Resultado", false },{"Mensaje", "" } };
+
+            try
+            {
+                using (DL.LGallegosEmisonEntities context = new DL.LGallegosEmisonEntities())
+                {
+                    var query = context.GetAllEmisor().ToList();
+                    if (query != null)
+                    {
+                        emisor.Emisores = new List<object>();
+                        foreach( var objEmisor in query )
+                        {
+                            ML.Emisor e = new ML.Emisor();
+                            e.IdEmisor = objEmisor.IdEmisor;
+                            e.RFC= objEmisor.RFC;
+                            e.FechaInicioOperacion = objEmisor.FechaInicioOperacion.Value;
+                            e.Capital = objEmisor.Capital.Value;
+                            emisor.Emisores.Add(e);
+                        }
+
+                        diccionario["Resultado"] = true;
+                        diccionario["Mensaje"] = "Se han cargado los datos";
+                        diccionario["Emisor"] = emisor;
+                    }
+                    else
+                    {
+                        diccionario["Mensaje"] = "N se han cargado los datos";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                diccionario["Mensaje"] = "N se han cargado los datos" + e;
+            }
+
+            return diccionario;
+        }
+
+
+        public static Dictionary<string, object> GetById(int idEmisor)
+        {
+            ML.Emisor emisor = new ML.Emisor();
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Emisor ", emisor }, { "Resultado", false }, { "Mensaje", "" } };
+
+            try
+            {
+                using (DL.LGallegosEmisonEntities context = new DL.LGallegosEmisonEntities())
+                {
+                    var query = context.GetByIdEmisor(idEmisor).SingleOrDefault();
+                    if (query != null)
+                    {
+
+                        emisor.IdEmisor = query.IdEmisor;
+                        emisor.RFC = query.RFC;
+                        emisor.FechaInicioOperacion = query.FechaInicioOperacion.Value;
+                        emisor.Capital = query.Capital.Value;
+                   
+                        diccionario["Resultado"] = true;
+                        diccionario["Mensaje"] = "Se han cargado los datos";
+                        diccionario["Emisor"] = emisor;
+                    }
+                    else
+                    {
+                        diccionario["Mensaje"] = "No se han cargado los datos";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                diccionario["Mensaje"] = "No se han cargado los datos" + e;
+            }
+
+            return diccionario;
+        }
+
+        public static Dictionary<string, object> Add(ML.Emisor emisor)
+        {
+           
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Resultado", false }, { "Mensaje", "" } };
+
+            try
+            {
+                using (DL.LGallegosEmisonEntities context = new DL.LGallegosEmisonEntities())
+                {
+                    var query = context.AddEmisor(emisor.RFC,emisor.FechaInicioOperacion,emisor.Capital);
+                    if (query > 0)
+                    {
+
+                       
+                        diccionario["Mensaje"] = "Se han guardado los datos";
+                        diccionario["Resultado"] = true;
+              
+                    }
+                    else
+                    {
+                        diccionario["Mensaje"] = "No se han cargado los datos";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                diccionario["Mensaje"] = "No se han cargado los datos" + e;
+            }
+
+            return diccionario;
+        }
+
+        public static Dictionary<string, object> Update(ML.Emisor emisor)
+        {
+
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Resultado", false }, { "Mensaje", "" } };
+
+            try
+            {
+                using (DL.LGallegosEmisonEntities context = new DL.LGallegosEmisonEntities())
+                {
+                    var query = context.UpdateEmisor(emisor.IdEmisor,emisor.RFC, emisor.FechaInicioOperacion, emisor.Capital);
+                    if (query > 0)
+                    {
+
+
+                        diccionario["Mensaje"] = "Se han Actualizado los datos";
+                        diccionario["Resultado"] = true;
+
+                    }
+                    else
+                    {
+                        diccionario["Mensaje"] = "No se han actualizado los datos";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                diccionario["Mensaje"] = "No se han actualizado los datos" + e;
+            }
+
+            return diccionario;
+        }
+
+        public static Dictionary<string, object> Delete(int idEmisor)
+        {
+
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Resultado", false }, { "Mensaje", "" } };
+
+            try
+            {
+                using (DL.LGallegosEmisonEntities context = new DL.LGallegosEmisonEntities())
+                {
+                    var query = context.DeleteEmisor(idEmisor);
+                    if (query > 0)
+                    {
+
+
+                        diccionario["Mensaje"] = "Se han borrado los datos";
+                        diccionario["Resultado"] = true;
+
+                    }
+                    else
+                    {
+                        diccionario["Mensaje"] = "No se han borrado los datos";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                diccionario["Mensaje"] = "No se han borrado los datos" + e;
+            }
+
+            return diccionario;
+        }
+
+
+    }
+
+
+
+}
---
+++ PL/Controllers/EmisorController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class EmisorController : Controller
+    {
+        // GET: Emisor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll ()
+        {
+            Dictionary<string,object> respuesta = BL.Emisor.GetAll();
+            bool resultado = (bool)respuesta["Resultado"];
+            string mensaje = (string)respuesta["Mensaje"];
+            if (resultado)
+            {
+                ML.Emisor emisor = (ML.Emisor)respuesta["Emisor"];
+
+                View(emisor);
+            }
+            else
+            {
+
+                ViewBag.Mensaje = mensaje;
+                PartialView("Modal");
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Form(ML.Emisor emisor)
+        {
+
+            if (emisor.IdEmisor > 0)
+            {
+                Dictionary<string, object> result = BL.Emisor.Update(emisor);
+                bool resultado = (bool)result["Resultado"];
+                if (resultado == true)
+                {
+                    ViewBag.Mensaje = "Se ha actualizado el registro";
+                    return PartialView("Modal");
+                }
+                else
+                {
+                    string exepcion = (string)result["Mensaje"];
+                    ViewBag.Mensaje = "El emisor no se pudo actualizar" + exepcion;
+                    return PartialView("Modal");
+                }
+            }
+            else
+            {
+                Dictionary<string, object> result = BL.Emisor.Add(emisor);
+                bool resultado = (bool)result["Resultado"];
+
+                if (resultado == true)
+                {
+                    ViewBag.Mensaje = "El emisor ha sido insertado";
+                    return PartialView("Modal");
+                }
+                else
+                {
+                    string exepcion = (string)result["Mensaje"];
+                    ViewBag.Mensaje = "El emisor no se pudo registrar" + exepcion;
+                    return PartialView("Modal");
+                }
+
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Form(int? idEmisor)
+        {
+
+            ML.Emisor emisor = new ML.Emisor();
+            if (idEmisor != null)
+            {
+                Dictionary<string, object> result = BL.Emisor.GetById(idEmisor.Value);
+                bool resultado = (bool)result["Resultado"];
+
+                if (resultado == true)
+                {
+                   
+                    emisor = (ML.Emisor)result["Emisor"];
+                    View(emisor);
+
+                }
+                else
+                {
+                    string exepcion = (string)result["Mensaje"];
+                    ViewBag.Mensaje = "Ocurrio un error al recuperar la informacion" + exepcion;
+                    return PartialView("Modal");
+                }
+            }
+            else
+            {
+                
+                return View(emisor);
+            }
+           
+
+            return View();
+       
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int idEmisor)
+        {
+            Dictionary<string, object> respuesta = BL.Emisor.Delete(idEmisor);
+            bool resultado = (bool)respuesta["Resultado"];
+            string mensaje = (string)respuesta["Mensaje"];
+            if (resultado == true)
+            {
+
+                ViewBag.Mensaje = mensaje;
+                return PartialView("Modal");
+            }
+            else
+            {
+
+                ViewBag.Mensaje = mensaje;
+                return PartialView("Modal");
+            }
+   
+        }
+
+
+    }
+}
